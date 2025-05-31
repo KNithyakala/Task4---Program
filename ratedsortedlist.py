35891ae2aaf5309fcb8c11d62775e9a363928e31
KNithyakala <cknithyakala@gmail.com>
2025-05-31 22:44:34 +0530

    Task 4 - Question 8

---
+++ ratedsortedlist.py
@@ -0,0 +1,10 @@
+#Question --8
+#Create a rated and sorted list
+#find minimum element in rated and sorted list
+
+
+Ratedlist=[("Task1",4.01),("Task2",10),("Task1",4.002),("Task3",8)]
+
+Ratedlist.sort()
+print("Rated and Sorted List:", Ratedlist)
+print("Minimum element in rated and sorted List:",Ratedlist[0])
